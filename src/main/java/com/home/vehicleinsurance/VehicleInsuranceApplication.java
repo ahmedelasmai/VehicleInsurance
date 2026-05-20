841b7172c3794855133b7ed655700396dcd3e4b4
Ahmed <ahmed.asmai@mastek.com>
2026-05-20 14:48:11 +0100

    Initial Spring Boot project setup

---
+++ src/main/java/com/home/vehicleinsurance/VehicleInsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.home.vehicleinsurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleInsuranceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleInsuranceApplication.class, args);
+    }
+
+}

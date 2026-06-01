539f80780f9c115f78e991b75fd83af79c0fc128
Ahmed <ahmed.asmai@mastek.com>
2026-06-01 11:36:09 +0100

    feat: added dummy data

---
+++ src/main/java/com/home/vehicleinsurance/service/TempVehicleDataService.java
@@ -0,0 +1,17 @@
+package com.home.vehicleinsurance.service;
+
+import com.home.vehicleinsurance.entity.Vehicle;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TempVehicleDataService {
+
+    public List<Vehicle> getVehicles() {
+        return List.of(
+                new Vehicle(1L, "ABC123", "Ahmed", "Car"),
+                new Vehicle(2L, "XYZ789", "John", "Truck")
+        );
+    }
+}
